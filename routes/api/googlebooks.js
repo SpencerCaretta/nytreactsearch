d44c6142cb03c1ee35804f6b7af54e001c932dd4
Spencer Caretta <spencer.caretta@gmail.com>
2019-02-23 22:34:45 -0600

    added a route to be able to query google books API

---
+++ routes/api/googlebooks.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+router.get("/:query", (req,res) => {
+    axios
+      .get("https://www.googleapis.com/books/v1/volumes?q=" + req.params.query)
+      .then((results) => res.json(results.data))
+      .catch(err => res.status(422).json(err));
+  });
+  
+  module.exports = router;
